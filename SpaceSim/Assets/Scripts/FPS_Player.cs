1feda277b8c30e57ecc2510bdc955017934aa8b3
Kouta <kouta@koutacles.com.au>
2016-03-01 12:04:35 +1100

    Initial commit

---
+++ SpaceSim/Assets/Scripts/FPS_Player.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Modified from Unity Standard Assets
+
+[RequireComponent (typeof (CharacterController))]
+public class FPS_Player: MonoBehaviour {
+
+	public float fWalkSpeed = 6.0f;
+	public float fRunSpeed = 11.0f;
+	public bool bLimitDiagonalSpeed = true;
+	public bool bRunToggleMode = false;
+	public float fJumpSpeed = 8.0f;
+	public float fGravity = 20.0f;
+	public float fFallDamageThreshold = 10.0f;
+	public bool bSlideWhenOverSlopeLimit = false;
+	//Uses "Slide" tag
+	public bool bSlideOnTaggedObjects = false;
+	public float fSlideSpeed = 12.0f;
+	public bool bAirControl = false;
+	public float fAntiSlopeStutter = 0.75f;
+	public int iAntiBunnyHop = 1;
+	public bool bFrozen = false;
+
+	private Vector3 vMoveDirection = Vector3.zero;
+	private bool bGrounded = false;
+	private CharacterController controller;
+	private Transform localtrans;
+	private float fSpeed;
+	private RaycastHit hit;
+	private float fFallStartLevel;
+	private bool bFalling;
+	private float fSlideLimit;
+	private float fRayLength;
+	private Vector3 vContactPoint;
+	private bool bPlayerControl = false;
+	private int iJumpTimer;
+
+	void Start() {
+		controller = GetComponent<CharacterController>();
+		localtrans = transform;
+		fSpeed = fWalkSpeed;
+		fRayLength = controller.height * 0.5f + controller.radius;
+		fSlideLimit = controller.slopeLimit - 0.1f;
+		iJumpTimer = iAntiBunnyHop;
+	}
+
+	void FixedUpdate() {
+		float inputX = 0.0f;
+		float inputY = 0.0f;
+		float inputModifyFactor = 1.0f;
+
+		if (!bFrozen) {
+			inputX = Input.GetAxis("Horizontal");
+			inputY = Input.GetAxis("Vertical");
+			//Diagonal movement limit
+			inputModifyFactor = (inputX != 0.0f && inputY != 0.0f && bLimitDiagonalSpeed) ? 0.7071f : 1.0f;
+		}
+
+		if (bGrounded) {
+			bool bSliding = false;
+			//Should we slide?
+			if (Physics.Raycast(localtrans.position, -Vector3.up, out hit, fRayLength)) {
+				if (Vector3.Angle(hit.normal, Vector3.up) > fSlideLimit) {
+					bSliding = true;
+				}
+			}
+			//Double check straight down for really steep slopes
+			else {
+				Physics.Raycast(vContactPoint + Vector3.up, -Vector3.up, out hit);
+				if (Vector3.Angle(hit.normal, Vector3.up) > fSlideLimit) {
+					bSliding = true;
+				}
+			}
+
+			//Fall damage
+			if (bFalling) {
+				bFalling = false;
+				if (localtrans.position.y < fFallStartLevel - fFallDamageThreshold) {
+					TakeFallingDamage(fFallStartLevel - localtrans.position.y);
+				}
+			}
+
+			//Simple run speed if toggle mode is disabled
+			if (!bRunToggleMode) {
+				fSpeed = Input.GetButton("Run") ? fRunSpeed : fWalkSpeed;
+			}
+
+			//Sliding (if enabled)
+			if ((bSliding && bSlideWhenOverSlopeLimit) || (bSlideOnTaggedObjects && hit.collider.tag == "Slide")) {
+				Vector3 hitNormal = hit.normal;
+				vMoveDirection = new Vector3(hitNormal.x, -hitNormal.y, hitNormal.z);
+				Vector3.OrthoNormalize (ref hitNormal, ref vMoveDirection);
+				vMoveDirection *= fSlideSpeed;
+				bPlayerControl = false;
+			}
+			//Anti-bump
+			else {
+				vMoveDirection = new Vector3(inputX * inputModifyFactor, -fAntiSlopeStutter, inputY * inputModifyFactor);
+				vMoveDirection = localtrans.TransformDirection(vMoveDirection) * fSpeed;
+				bPlayerControl = true;
+			}
+
+			//Jump
+			if (!Input.GetButton("Jump")) {
+				iJumpTimer++;
+			}
+			else if (iJumpTimer >= iAntiBunnyHop) {
+				vMoveDirection.y = fJumpSpeed;
+				iJumpTimer = 0;
+			}
+		}
+		//In the air
+		else {
+			//Start counting fall height
+			if (!bFalling) {
+				bFalling = true;
+				fFallStartLevel = localtrans.position.y;
+			}
+
+			//Air control
+			if (bAirControl && bPlayerControl) {
+				vMoveDirection.x = inputX * fSpeed * inputModifyFactor;
+				vMoveDirection.z = inputY * fSpeed * inputModifyFactor;
+				vMoveDirection = localtrans.TransformDirection(vMoveDirection);
+			}
+		}
+
+		//Apply gravity
+		vMoveDirection.y -= fGravity * Time.deltaTime;
+
+		//Grounded?
+		bGrounded = (controller.Move(vMoveDirection * Time.deltaTime) & CollisionFlags.Below) != 0;
+
+		//Rocket Booster quickie
+		if (Input.GetKey(KeyCode.Space)) {
+			vMoveDirection.y = 5.0f;
+		}
+	}
+
+	void Update() {
+		//If the run button is set to toggle switch between walk/run speed
+		if (bRunToggleMode && bGrounded && Input.GetButtonDown("Run"))
+			fSpeed = (fSpeed == fWalkSpeed? fRunSpeed : fWalkSpeed);
+	}
+		
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		vContactPoint = hit.point;
+	}
+		
+	void TakeFallingDamage(float fallDistance) {
+		print ("Fell " + fallDistance);   
+	}
+}
---
+++ SpaceSim/Assets/Scripts/RandomQuadTerrain.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomQuadTerrain : MonoBehaviour {
+
+	private MeshRenderer rend;
+	private MeshCollider collide;
+	private MeshFilter filter;
+
+	private MeshBuilder construct;
+
+	public int m_SegmentCount = 10;
+	public int m_Width = 10;
+	public int m_Length = 10;
+	public int m_MaxHeight = 15;
+	public int m_MinHeight = -30;
+	public int m_StepCount = 10;
+
+	public float mountain_chance = 25.00f;
+	public float plain_chance = 1.00f;
+
+	// Use this for initialization
+	void Start () {
+		rend = this.GetComponent<MeshRenderer>();
+		collide = this.GetComponent<MeshCollider>();
+		filter = this.GetComponent<MeshFilter>();
+		construct = new MeshBuilder();
+
+		Generate();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.M)) {
+			construct = new MeshBuilder();
+			Generate();
+		}
+	}
+
+	public void BuildQuad(MeshBuilder meshBuilder, Vector3 offset) {
+		meshBuilder.Vertices.Add(new Vector3(0.0f, 0.0f, 0.0f) + offset);
+		meshBuilder.UVs.Add(new Vector2(0.0f, 0.0f));
+		meshBuilder.Normals.Add(Vector3.up);
+
+		meshBuilder.Vertices.Add(new Vector3(0.0f, 0.0f, m_Length) + offset);
+		meshBuilder.UVs.Add(new Vector2(0.0f, 1.0f));
+		meshBuilder.Normals.Add(Vector3.up);
+
+		meshBuilder.Vertices.Add(new Vector3(m_Width, 0.0f, m_Length) + offset);
+		meshBuilder.UVs.Add(new Vector2(1.0f, 1.0f));
+		meshBuilder.Normals.Add(Vector3.up);
+
+		meshBuilder.Vertices.Add(new Vector3(m_Width, 0.0f, 0.0f) + offset);
+		meshBuilder.UVs.Add(new Vector2(1.0f, 0.0f));
+		meshBuilder.Normals.Add(Vector3.up);
+
+		int baseIndex = meshBuilder.Vertices.Count - 4;
+
+		meshBuilder.AddTriangle(baseIndex, baseIndex + 1, baseIndex + 2);
+		meshBuilder.AddTriangle(baseIndex, baseIndex + 2, baseIndex + 3);
+	}
+
+	void BuildQuadForGrid(MeshBuilder meshBuilder, Vector3 position, Vector2 uv, bool buildTriangles, int vertsPerRow) {
+		meshBuilder.Vertices.Add(position);
+		meshBuilder.UVs.Add(uv);
+
+		if (buildTriangles) {
+			int baseIndex = meshBuilder.Vertices.Count - 1;
+
+			int index0 = baseIndex;
+			int index1 = baseIndex - 1;
+			int index2 = baseIndex - vertsPerRow;
+			int index3 = baseIndex - vertsPerRow - 1;
+
+			meshBuilder.AddTriangle(index0, index2, index1);
+			meshBuilder.AddTriangle(index2, index3, index1);
+		}
+	}
+
+	public void Generate() {
+		int iKeepHigh = 0;
+		int iKeepFlat = 0;
+
+		for (int i = 0; i <= m_SegmentCount; i++) {
+			float z = m_Length * i;
+			float v = (1.0f / m_SegmentCount) * i;
+
+			for (int j = 0; j <= m_SegmentCount; j++) {
+				float x = m_Width * j;
+				float u = (1.0f / m_SegmentCount) * j;
+
+				if (iKeepHigh > 0) {
+					iKeepHigh--;
+				}
+
+				if (iKeepFlat > 0) {
+					iKeepFlat--;
+				}
+
+				float finalheight = Random.Range(m_MinHeight, m_MaxHeight);
+				float ismount = Random.Range(0.0f, 100.0f);
+				float isplain = Random.Range(0.0f, 100.0f);
+
+				//Mountains
+				if (ismount < mountain_chance) {
+					finalheight *= Random.Range(1, 6);
+					finalheight = Mathf.Clamp(finalheight, 0, Mathf.Infinity);
+
+					iKeepHigh += Random.Range(4, 16);
+				}
+
+				if (iKeepHigh > 0) {
+					finalheight *= Random.Range(1, 3);
+					finalheight = Mathf.Clamp(finalheight, 0, Mathf.Infinity);
+				}
+
+				//Plains
+				if (isplain < plain_chance && iKeepHigh == 0) {
+					finalheight = 0;
+					finalheight += Random.Range(-1.0f, 1.0f);
+
+					iKeepFlat += Random.Range(4, 16);
+				}
+
+				if (iKeepFlat > 0) {
+					finalheight = 0;
+					finalheight += Random.Range(-1.0f, 1.0f);
+				}
+
+				Vector3 offset = new Vector3(x, finalheight, z);
+
+				Vector2 uv = new Vector2(u, v);
+				bool buildTriangles = i > 0 && j > 0;
+
+				BuildQuadForGrid(construct, offset, uv, buildTriangles, m_StepCount + 1);
+			}
+		}
+
+		filter.mesh = construct.CreateMesh();
+		filter.mesh.RecalculateNormals();
+		collide.sharedMesh = filter.mesh;
+	}
+}
