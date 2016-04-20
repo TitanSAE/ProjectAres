d5946506d661791d70634dd42df9bd5acb050651
unknown <Kediza@S-803-09-1227-W.sae.au>
2016-04-20 14:54:52 +1000

    Small Updates

--- MarsPrototype/Assets/Scripts/RoverModules/RechargePad.cs
+++ MarsPrototype/Assets/Scripts/RoverModules/RechargePad.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class RechargePad : MonoBehaviour {
-
+	bool onPad = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,13 +10,22 @@ public class RechargePad : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKey (KeyCode.C) && onPad) {
+			GameObject.Find ("SceneManager").GetComponent<Fading> ().ResetFade ();
+			GameObject.Find ("SceneManager").GetComponent<Fading> ().BeginFade (-1);
+			Debug.Log ("hit");
+		}
 	}
 
 	void OnTriggerEnter(Collider col) {
-		if (col.tag == "Player") {
+		if (col.gameObject.tag == "Player") {
+			onPad = true;
+		}
+	}
 
-
+	void OnTriggerExit(Collider col) {
+		if (col.gameObject.tag == "Player") {
+			onPad = false;
 		}
 	}
 }
