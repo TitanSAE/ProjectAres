6f78a4ff8c5bdc50f3e2cca3d0aa8b59b7e24a3d
David Hanna <onnfire@hotmail.com>
2016-04-21 19:02:16 +1000

    Added Goal system

---
+++ MarsPrototype/Assets/Scripts/UI/GoalBuilderEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MarsMessageManager))]
+public class GoalBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        MarsMessageManager goalBuilder = (MarsMessageManager)target;
+        if(GUILayout.Button("Add Goal"))
+        {
+            goalBuilder.AddMessageWithoutAddingToList();
+        }
+    }
+}
