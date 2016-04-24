72a018a821813fe951dc933b6dea914cff42ab73
Kouta <kouta@koutacles.com.au>
2016-04-24 14:46:34 +1000

    Made Main Menu work Tweaked some save/load data oddities Stay on main menu for 1 minute for a happy time

---
+++ MarsPrototype/Assets/MainMenuSceneManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuSceneManager : MonoBehaviour {
+
+	public AudioSource asHappy;
+	private float fTime;
+	public float fHappyTime = 60;
+	private bool bHappy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fTime += Time.deltaTime;
+
+		if (fTime >= fHappyTime && !bHappy) {
+			asHappy.Play();
+			bHappy = true;
+		}
+	}
+
+	public void NextScene() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene("sponsor_select");
+	}
+
+	public void QuitGame() {
+		if (!Application.isEditor) {
+			UnityEngine.Application.Quit();
+		}
+
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+	}
+}
