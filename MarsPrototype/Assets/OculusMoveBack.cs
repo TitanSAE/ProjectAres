298aebaa2c729b572323c0b08ef9f8541af767cf
Kouta <kouta@koutacles.com.au>
2016-05-03 10:12:18 +1000

    More button icons and button icons switch depending if controller is plugged in scene lighting tweaks new rover code

---
+++ MarsPrototype/Assets/OculusMoveBack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OculusMoveBack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (UnityEngine.VR.VRDevice.isPresent) {
+			this.transform.position = new Vector3(0, 0, -9.5f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
