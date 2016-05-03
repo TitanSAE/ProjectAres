c47613feeace9553e567f59c80917b3a49e7e1ce
Kouta <kouta@koutacles.com.au>
2016-05-03 11:58:06 +1000

    More messages

--- MarsPrototype/Assets/Scripts/TimedDelivery.cs
+++ MarsPrototype/Assets/Scripts/TimedDelivery.cs
@@ -51,31 +51,31 @@ public class TimedDelivery : MonoBehaviour {
 					SendPackage();
 				}
 				else if (eMission == GAME_QUESTS.PACKAGE_2) {
-					mngMessages.AddMessage("Delivery Notification", "We've sent some more building supplies. Hopefully they land nearby!", "sponsor");
+					//mngMessages.AddMessage("Delivery Notification", "We've sent some more building supplies. Hopefully they land nearby!", "sponsor");
 					fCountdown = 60.0f;
 
 					SendPackage1();
 				}
 				else if (eMission == GAME_QUESTS.PACKAGE_3) {
-					mngMessages.AddMessage("Delivery Notification", "We've sent some more building supplies. Hopefully they land nearby!", "sponsor");
+					//mngMessages.AddMessage("Delivery Notification", "We've sent some more building supplies. Hopefully they land nearby!", "sponsor");
 					fCountdown = 120.0f;
 
 					SendPackage2();
 				}
 				else if (eMission == GAME_QUESTS.PACKAGE_4) {
-					mngMessages.AddMessage("Delivery Notification", "We've sent some more building supplies. Hopefully they land nearby!", "sponsor");
+					//mngMessages.AddMessage("Delivery Notification", "We've sent some more building supplies. Hopefully they land nearby!", "sponsor");
 					fCountdown = 120.0f;
 
 					SendPackage();
 				}
 				else if (eMission == GAME_QUESTS.PACKAGE_5) {
-					mngMessages.AddMessage("Delivery Notification", "We've sent some more building supplies. Hopefully they land nearby!", "sponsor");
+					//mngMessages.AddMessage("Delivery Notification", "We've sent some more building supplies. Hopefully they land nearby!", "sponsor");
 					fCountdown = 300.0f;
 
 					SendPackage1();
 				}
 				else if (eMission == GAME_QUESTS.PACKAGE_6) {
-					mngMessages.AddMessage("Delivery Notification", "We've sent some more building supplies. Hopefully they land nearby!", "sponsor");
+					//mngMessages.AddMessage("Delivery Notification", "We've sent some more building supplies. Hopefully they land nearby!", "sponsor");
 					fCountdown = 300.0f;
 
 					SendPackage2();
