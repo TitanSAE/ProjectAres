acbe3f362093adc6703c6ee4a14553c001b51b2e
unknown <dhanna1@S-803-11-1229-W.sae.au>
2016-05-10 09:18:02 +1000

    UI Stuff changed

---
+++ MarsPrototype/Assets/Scripts/UI/UIFollow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIFollow : MonoBehaviour {
+
+    [Header("Position")]
+    public bool bFollowPosition = true;
+    public bool bFollowRotation = false;
+    public Transform tObjectToFollow;
+    public Rect RectTMyOwn;
+    
+
+    void Update()
+    {
+        //This is used to make the object follow the object of your choice.
+        if (bFollowPosition)
+        {
+            
+        }
+
+        //This is used to make the object rotate in sync with the object of your chocie.
+        if (bFollowRotation)
+        {
+            Vector3 ZRotOnly = new Vector3(0, 0, -tObjectToFollow.localRotation.eulerAngles.y);
+            //RectTMyOwn.rotation = Quaternion.Euler(ZRotOnly);
+        }
+    }
+}
